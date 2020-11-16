cf7b34b88995ee2ccc36892b2dfd43780804b94a
Pasha Teroshyn <1tymorrow1@gmail.com>
2020-11-16 21:55:35 +0100

    Add_Reverse

--- MySite/urls.py
+++ MySite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home, name= 'Home'),
     path('about/', views.about, name= 'about'),
+    path('revers/', views.revers, name= 'revers'),
 ]
--- MySite/views.py
+++ MySite/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render 
 
-def Home(requst):
-	return render (requst,'Home.html')
+def Home(request):
+	return render (request,'Home.html')
 
 def about(requst):
 	return HttpResponse('It"s about')
+
+def revers(request):
+	text_revers = request.GET['usertext']
+	return render (request,'revers.html', {'usertext':text_revers})
